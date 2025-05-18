cba9abf6821738e43e1732d66aebf2429a278017
Wellington <wellington-158@hotmail.com>
2025-05-17 22:31:48 -0300

    fix: missing cors

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,7 @@ async function bootstrap() {
 		}),
 	});
 
+	app.enableCors();
 	app.setGlobalPrefix('api');
 	app.useGlobalFilters(new CatchErrorMiddleware());
 	app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
